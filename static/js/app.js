0a18f880a1d2e92155d3bc5fa47ba0eba24f9c6a
Tianwei Liu <tianwei.liu89@gmail.com>
2015-10-26 20:41:22 -0500

    use proper async loading, close #3

--- static/js/app.js
+++ static/js/app.js
@@ -1,152 +1,28 @@
 var app = angular.module('wows-stats', []);
 
-app.controller('TeamStatsCtrl', function ($scope, $http) {
-	$scope.inGame = false;
-	$scope.dateTime = "";
-  	$scope.data = {};
-  	$scope.friendly = [];
-  	$scope.foe = [];
-  	$scope.queue = [];
-  	$scope.processing = [];
-	var updateArea = function() {
-		$http({
-			method: 'GET',
-			url: 'http://localhost:8080/api/arena'
-		}).success(function(data, status) {
-			$scope.inGame = true;
-			$scope.data = data;
-			if ($scope.dateTime != data.dateTime) {
-				// new game
-				$scope.dateTime = data.dateTime;
-				angular.forEach(data.vehicles, function(value, key) {
-					$http({
-						method:'GET',
-						url: 'http://localhost:8080/api/player?name=' + encodeURIComponent(value.name)
-					}).success(function(stats, statsStatus) {
-						var player = {};
-						angular.extend(player, value);
-						angular.extend(player, stats);
-						process(player);
-						$http({
-							method:'GET',
-							url: 'http://localhost:8080/api/ship?playerId=' + player.id + '&shipId=' + value.shipId
-						}).success(function(shipStats, shipStatsStatus) {
-							var battles = parseInt(shipStats.battles);
-							var victories = parseInt(shipStats.victories);
-							var winRate = (victories / battles * 100).toFixed(2);
-							var survived = parseInt(shipStats.survived);
-							var kill = parseInt(shipStats.destroyed);
-							var death = battles - survived;
-							var kdRatio = (kill / death).toFixed(2);
-							player.ship = {
-								"name": shipStats.name,
-								"img": shipStats.img,
-								"winRate": winRate + "%",
-								"winRateClass": beautify("winRate", winRate),
-								"kdRatio": kdRatio,
-								"battles": battles,
-								"avgExp": shipStats.avgExp,
-								"avgDmg": shipStats.avgDmg
-							}
-							if (value.relation == 2) {
-								$scope.foe.push(player);
-							}
-							else {
-								$scope.friendly.push(player);
-							}
-						}).error(function(stats, statsStatus) {
-							if ($scope.queue.indexOf(value) == -1)
-								$scope.queue.push(value);
-						});
-						//console.log(player);
-					}).error(function(data, status) {
-						if ($scope.queue.indexOf(value) == -1)
-							$scope.queue.push(value);
-						/*
-						var player = {};
-						angular.extend(player, value);
-						if (value.relation == 2) {
-							$scope.foe.push(player);
-						}
-						else {
-							$scope.friendly.push(player);
-						}
-						console.log(player);
-						*/
-					});
-				});
-			}
-			else if ($scope.queue.length > 0) {
-				var queue = [];
-				angular.copy($scope.queue, queue);
-				angular.forEach(queue, function(value, key) {
-					if ($scope.processing.indexOf(value) != -1)
-						return;
-					else
-						$scope.processing.push(value);
-					console.log(value);
-					$http({
-						method:'GET',
-						url: 'http://localhost:8080/api/player?name=' + encodeURIComponent(value.name)
-					}).success(function(stats, statsStatus) {
-						var player = {};
-						angular.extend(player, value);
-						angular.extend(player, stats);
-						process(player);
-						$http({
-							method:'GET',
-							url: 'http://localhost:8080/api/ship?playerId=' + player.id + '&shipId=' + value.shipId
-						}).success(function(shipStats, shipStatsStatus) {
-							var battles = parseInt(shipStats.battles);
-							var victories = parseInt(shipStats.victories);
-							var winRate = (victories / battles * 100).toFixed(2) + "%";
-							var survived = parseInt(shipStats.survived);
-							var kill = parseInt(shipStats.destroyed);
-							var death = battles - survived;
-							var kdRatio = (kill / death).toFixed(2);
-							player.ship = {
-								"name": shipStats.name,
-								"img": shipStats.img,
-								"winRate": winRate,
-								"winRateClass": beautify("winRate", winRate),
-								"kdRatio": kdRatio,
-								"battles": battles,
-								"avgExp": shipStats.avgExp,
-								"avgDmg": shipStats.avgDmg
-							}
-							if (value.relation == 2) {
-								$scope.foe.push(player);
-							}
-							else {
-								$scope.friendly.push(player);
-							}
-							$scope.processing.splice($scope.processing.indexOf(value), 1);
-							$scope.queue.splice($scope.queue.indexOf(value), 1);
-						}).error(function(stats, statsStatus) {
-							$scope.processing.splice($scope.processing.indexOf(value), 1);
-						});
-						//console.log(player);
-					});
-				});
-			}
-
-		}).error(function(data, status) {
-			$scope.dateTime = "";
-			$scope.inGame = false;
-			$scope.data = {};
-			$scope.friendly = [];
-		  	$scope.foe = [];
-		  	$scope.queue = [];
+app.factory('api', function($http, $q) {
+	var api = {};
+	api.fetchShip = function(player) {
+		player.api.ship = api.ship(player);
+		player.api.ship.then(function(player){
+			// nothing needs to be done after fetching ship stats
+		}, function(player){
+			// retry if rejected
+			api.fetchShip(player);
 		});
 	}
-
-	function process(player) {
-		player.uri = player.id + '-' + encodeURIComponent(player.name);
-		var winRate = parseFloat(player.winRate.replace('%', ''));
-		player.winRateClass = beautify("winRate", winRate);
+	api.fetchPlayer = function(player) {
+		player.api.player = api.player(player);
+		player.api.player.then(function(player) {
+			// fetch ship stats after player fetching is done so we have the proper playerId
+			api.fetchShip(player);
+		}, function(player) {
+			// retry if rejected
+			api.fetchPlayer(player);
+		});
 	}
-
-	function beautify(type, value) {
+	api.beautify = function(type, value) {
+		// xvm like colorization
 		switch(type) {
 			case "winRate":
 				if (value < 47) {
@@ -172,6 +48,82 @@ app.controller('TeamStatsCtrl', function ($scope, $http) {
 				return null;
 				break;
 		}
+	}
+	api.player = function(player) {
+		return $q(function(resolve, reject) {
+			$http({
+				method:'GET',
+				url: 'http://localhost:8080/api/player?name=' + encodeURIComponent(player.name)
+			}).success(function(data, status) {
+				angular.extend(player, data);
+				player.uri = player.id + '-' + encodeURIComponent(player.name);
+				var winRate = parseFloat(player.winRate.replace('%', ''));
+				player.winRateClass = api.beautify("winRate", winRate);
+				resolve(player);
+			}).error(function(data, status) {
+				reject(player);
+			});
+		});
+	}
+	api.ship = function(player) {
+		return $q(function(resolve, reject) {
+			$http({
+				method:'GET',
+				url: 'http://localhost:8080/api/ship?playerId=' + player.id + '&shipId=' + player.shipId
+			}).success(function(data, status) {
+				var battles = parseInt(data.battles);
+				var victories = parseInt(data.victories);
+				var winRate = (victories / battles * 100).toFixed(2);
+				var survived = parseInt(data.survived);
+				var kill = parseInt(data.destroyed);
+				var death = battles - survived;
+				var kdRatio = (kill / death).toFixed(2);
+				player.ship = {
+					"name": data.name,
+					"img": data.img,
+					"winRate": winRate + "%",
+					"winRateClass": api.beautify("winRate", winRate),
+					"kdRatio": kdRatio,
+					"battles": battles,
+					"avgExp": data.avgExp,
+					"avgDmg": data.avgDmg
+				}
+				resolve(player);
+			}).error(function(data, status) {
+				reject(player);
+			});
+		});
+	}
+	return api;
+});
+
+app.controller('TeamStatsCtrl', function ($scope, $http, api) {
+	$scope.inGame = false;
+	$scope.dateTime = "";
+  	$scope.data = {};
+  	$scope.players = [];
+	var updateArea = function() {
+		$http({
+			method: 'GET',
+			url: 'http://localhost:8080/api/arena'
+		}).success(function(data, status) {
+			$scope.inGame = true;
+			$scope.data = data;
+			if ($scope.dateTime != data.dateTime) {
+				// new game
+				$scope.dateTime = data.dateTime;
+				for (var i=0; i<data.vehicles.length; i++) {
+					var player = data.vehicles[i];
+					$scope.players.push(player);
+					player.api = {};
+					api.fetchPlayer(player);
+				}
+			}
+		}).error(function(data, status) {
+			$scope.dateTime = "";
+			$scope.inGame = false;
+			$scope.data = {};
+		});
 	}
 
 	var timer = setInterval(function() {
